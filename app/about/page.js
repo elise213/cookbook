32f28170d03dbf4a157379bbfcd66846cf98db59
Mara <maracodes213@gmail.com>
2024-09-27 18:40:40 -0700

    saving

--- app/about/page.js
+++ app/about/page.js
@@ -29,6 +29,26 @@ const About = () => {
             culpa qui officia deserunt mollit anim id est laborum
           </p>
         </div>
+        <div className="about-person">
+          <Image
+            height={100}
+            width={100}
+            layout="responsive"
+            quality={100}
+            className="person-image"
+            src="/img/TH1.png"
+            alt=""
+          ></Image>
+          <p className="person-bio">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
+        </div>
 
         <Footer />
       </div>
