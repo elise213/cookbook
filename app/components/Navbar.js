93c3e4ff38e7b2bed3f27485951634533982913b
Mara <maracodes213@gmail.com>
2025-03-05 16:55:12 -0800

    m

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -9,27 +9,6 @@ const Navbar = () => {
   const [visible, setVisible] = useState(true);
   const prevScrollPos = useRef(0);
 
-  const [overflowingElements, setOverflowingElements] = useState([]);
-
-  useEffect(() => {
-    const checkOverflow = () => {
-      const elements = document.querySelectorAll("*");
-      const overflowElements = [];
-      elements.forEach((el) => {
-        if (el.scrollWidth > window.innerWidth) {
-          overflowElements.push(el);
-        }
-      });
-      setOverflowingElements(overflowElements);
-      if (overflowElements.length > 0) {
-        console.warn("Overflowing elements detected:", overflowElements);
-      }
-    };
-    checkOverflow();
-    window.addEventListener("resize", checkOverflow);
-    return () => window.removeEventListener("resize", checkOverflow);
-  }, []);
-
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollPos = window.scrollY;
