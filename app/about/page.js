cc42a66bf2cd5dd881799ed033471ddc90c69932
Mara <maracodes213@gmail.com>
2025-08-06 01:33:43 -0700

    s

--- app/about/page.js
+++ app/about/page.js
@@ -19,25 +19,26 @@ const About = () => {
               {isArabic ? (
                 <>
                   <p>
-                    وُلِدت فاطمة في مدينة رفح، في مجتمع مليء بالتقاليد العريقة
-                    والطعام الشهي. كانت تطهو مع أطفالها الوصفات التي تعلمتها من
-                    والدتها وجدتها — كل طبق يحمل إرثاً فلسطينياً عميقاً، ليس من
-                    تأليف شخص واحد، بل وُلد من أرض فلسطين.
+                    نشأت فاطمة في مجتمع غني بالتقاليد العريقة والطعام الشهي في
+                    غزة. في مدينة رفح، كانت تطهو مع أطفالها الوصفات التي تعلمتها
+                    من والدتها وجدتها — حيث يحمل كل طبق إرثاً عميقاً، لم يكتبه
+                    شخص واحد، بل وُلد من أرض فلسطين.
                   </p>
                   <p>
-                    بعد أن قُصف منزلها، فرت فاطمة من غزة مع أطفالها وزوجها. هذا
-                    الكتاب هو احتفاء بالتراث الفلسطيني، وتحية لكل من حافظ عليه
-                    رغم كل الصعاب.
+                    بعد أن قُصف منزلها، اضطرت فاطمة للفرار من غزة مع أطفالها
+                    وزوجها. هذا الكتاب هو احتفاء بالتراث الفلسطيني، وتحية لكل من
+                    حافظ عليه رغم كل الصعاب.
                   </p>
                 </>
               ) : (
                 <>
                   <p>
-                    Fatima was grew up in a community full of rich traditions
-                    and delicious food in Gaza. In Rafah she would cook with her
-                    children, therecipes she learned from her mother and
-                    grandmother — each dish carrying a legacy. Not written by
-                    any one person, but born of the land of Palestine.
+                    Fatima grew up in a community full of rich traditions and
+                    delicious food in Gaza. In Rafah, she would cook with her
+                    children the recipes she learned from her mother and
+                    grandmother — each dish carrying a legacy. The recipes
+                    weren’t written by any one person, but were born of the land
+                    of Palestine.
                   </p>
                   <p>
                     After her home was bombed, Fatima fled Gaza with her
@@ -66,7 +67,7 @@ const About = () => {
                   <>
                     <p>
                       When you purchase the cookbook, you are supporting
-                      Fatima's family in Gaza, and helping her children to
+                      Fatima's parents in Gaza, and helping her children to
                       attend school in Egypt.
                     </p>
                   </>
